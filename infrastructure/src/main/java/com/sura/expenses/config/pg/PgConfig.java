cb5106cb0cb3b70a26df923ba4e54cf88308efce
Tomas Salinas <52550075+simonzm7@users.noreply.github.com>
2022-03-30 23:31:13 -0500

    SURA: DB Connection

---
+++ infrastructure/src/main/java/com/sura/expenses/config/pg/PgConfig.java
@@ -0,0 +1,4 @@
+package com.sura.expenses.config.pg;
+
+public class PgConfig {
+}
--- infrastructure/src/main/java/com/sura/expenses/employees/entity/Employee.java
+++ infrastructure/src/main/java/com/sura/expenses/employees/entity/Employee.java
@@ -1,10 +1,36 @@
 package com.sura.expenses.employees.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
 @Table(name = "employee")
+@Data
 public class Employee {
+
+
+    @Id
+    @SequenceGenerator(
+            name = "employee_sequence",
+            sequenceName = "employee_sequence",
+            allocationSize = 1
+
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "employee_sequence"
+    )
+    private Long id;
+
+    @Column(name = "employee_id", unique = true)
+    private Long employeeId;
+
+    @Column(name = "employee_name")
+    private String employeeName;
+
+    @Column(name="create_At", columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Date createAt;
 
 }
